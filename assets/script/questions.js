ffb0f75b1741486f2a5e3980efbee1d60f448971
SKhai77 <orionkhai@gmail.com>
2023-06-27 19:32:53 -0500

    Update questions.js file

--- assets/script/questions.js
+++ assets/script/questions.js
@@ -1,6 +1,6 @@
 
 
-var quiz = [
+var quizData = [
     {
         question: "Commonly used data types DO NOT include:",
         choices: ["strings", "booleans", "alerts", "numbers"],
@@ -25,7 +25,64 @@ var quiz = [
         question: "A very useful tool used during development and debugging for printing content to the debugger is:",
         choices: ["JavaScript", "terminal/bash", "for loops", "console.log"],
         answer: "console.log"
-    }  
-]
+    }
+];
 
+var quizIndex = 0;
 
+function loadQuiz() {
+    var quizContainer = document.getElementById('container');
+
+    if (quizIndex >= quizData.length) {
+        quizContainer.innerHTML = '';
+        displayScore();
+        return;
+    }
+
+    var quizIndex = 0;
+
+    function loadQuiz() {
+        var quizContainer = document.getElementById('container');
+
+        // Check if all questions have been answered
+        if (quizIndex >= quizData.length) {
+            quizContainer.innerHTML = '';
+            displayScore();
+            return;
+        }
+
+        var currentQuestion = quizData[quizIndex];
+
+        // Create and display the question
+        var questionElement = document.createElement('h2');
+        questionElement.textContent = currentQuestion.question;
+        quizContainer.appendChild(questionElement);
+
+        var answerContainer = document.createElement('ol');
+
+        // Iterate through the answers for the current question
+        currentQuestion.answer.forEach((answer, _index) => {
+            var answerElement = document.createElement('li');
+            answerElement.textContent = answer;
+
+            // Event listener for answer selection
+            answerElement.addEventListener('click', () => {
+                quizIndex++; // Move to the next question
+                quizContainer.innerHTML = ''; // Clear the quiz container
+                loadQuiz(); // Load the next question
+            });
+
+            answerContainer.appendChild(answerElement);
+        });
+
+        quizContainer.appendChild(answerContainer);
+    }
+
+    function displayScore() {
+        var quizContainer = document.getElementById('container');
+        var scoreElement = document.createElement('ol');
+        var remainingSeconds = Math.max(0, timeLeft); // Ensure score is not negative
+        scoreElement.textContent = 'Your score is: ' + remainingSeconds;
+        quizContainer.appendChild(scoreElement);
+    }
+}
